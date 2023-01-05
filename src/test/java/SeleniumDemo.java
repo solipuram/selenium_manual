02f325e848dafc647816e46f342ea5f8743825af
Srinivas Reddy <solipuramsrinivas@gmail.com>
2023-01-05 12:31:26 +0000

    update

--- src/test/java/SeleniumDemo.java
+++ src/test/java/SeleniumDemo.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
