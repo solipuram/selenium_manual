e5c25ec7f426301bc8516246574fe935f4348cd5
Srinivas Reddy <solipuramsrinivas@gmail.com>
2023-01-05 12:55:54 +0000

    update

--- src/test/java/SeleniumDemo.java
+++ src/test/java/SeleniumDemo.java
@@ -1,52 +1,29 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 class SeleniumDemo {
-    private WebDriver driver;
-    
-    @BeforeClass
-    public void setUp()
-    {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver.navigate().to("https://the-internet.herokuapp.com/login");
-        driver.manage().window().maximize();
-        //driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        driver = WebDriverManager.chromedriver().create();
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
     }
 
     @Test
-    public void userLogin() {
-        WebElement usernameTxt = driver.findElement(By.id("username"));
-        usernameTxt.sendKeys("tomsmith");
-        WebElement passwordTxt = driver.findElement(By.id("password"));
-        passwordTxt.sendKeys("SuperSecretPassword!");
-        WebElement submitBtn = driver.findElement(By.className("radius"));
-        submitBtn.click();
-        System.out.println("Current URL is:" + driver.getCurrentUrl());
-        Assert.assertTrue(driver.getCurrentUrl().contains("secure"));
+    void test() {
+        driver.get("https://www.jenkins.io/");
+        assertThat(driver.getTitle()).contains("Jenkins");
     }
-    
-    @AfterClass
-    public void tearDown(){
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
 
 }
