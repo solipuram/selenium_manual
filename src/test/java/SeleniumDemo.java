ffb000a1f5e897996411b051c7931ca45a6bbbd6
Srinivas Reddy <solipuramsrinivas@gmail.com>
2023-01-05 13:45:01 +0000

    mvn file

--- src/test/java/SeleniumDemo.java
+++ src/test/java/SeleniumDemo.java
@@ -1,36 +1,37 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import static org.assertj.core.api.Assertions.assertThat;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 class SeleniumDemo {
 
-    WebDriver driver;
-
-    @BeforeAll
-    static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
+    public WebDriver driver; 
+    public String baseUrl = "https://www.lambdatest.com/";
 
 
-    @BeforeEach
-    void setup() {
-        driver = new ChromeDriver();
-    }
+    
     @Test
     void test() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);  
+        driver.manage().window().maximize();  
         driver.get("https://www.jenkins.io/");
         assertThat(driver.getTitle()).contains("srinivas");
+        driver.close();
+
+        
     }
 
-    @AfterEach
-    void teardown() {
-        driver.quit();
-    }
+    
 
    
 
